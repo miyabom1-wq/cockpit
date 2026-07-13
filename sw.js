893d603e4f81c8f9609cd7ffeee3ff77a2dc9db3
miyabom1-wq <miya.bom1@gmail.com>
2026-07-13 09:42:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vantage-selective-backtest-20260713-v30';
+const CACHE='vantage-jp-ranking-explorer-20260713-v32';
 const SHELL=['./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./icon-badge.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',e=>{
