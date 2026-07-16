6cec4140562a0bf4838aedde208053c938bfc4f7
miyabom1-wq <miyabom1-wq@users.noreply.github.com>
2026-07-17 08:20:46 +0900

    Link VANTAGE candidates to FRAME

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vantage-global-radar-20260717-v37';
+const CACHE='vantage-frame-handoff-20260717-v38';
 const SHELL=['./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./icon-badge.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>null).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
